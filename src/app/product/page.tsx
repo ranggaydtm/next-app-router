04d7bf5878b5a7139fef429acdb827760af290af
ranggaydtm <ranggay67@gmail.com>
2025-04-22 16:42:03 +0700

    create product page

---
+++ src/app/product/page.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type ProductProps = { params: { slug: string[] } };
+
+async function getData() {
+  //   const res = await fetch("https://fakestoreapi.com/products");
+  const res = await fetch("http://localhost:3000/api/product", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function ProductPage(props: ProductProps) {
+  const { params } = props;
+  const products = await getData();
+
+  console.log(products);
+  return (
+    <div className="grid grid-cols-4 place-items-center mt-5">
+      {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
+      {products.data.length > 0 &&
+        products.data.map((product: any) => (
+          <div
+            key={product.id}
+            className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 my-2"
+          >
+            <a href="#">
+              <img
+                className="p-8 rounded-t-lg object-cover h-96 w-full"
+                src={product.image}
+                alt="product image"
+              />
+            </a>
+            <div className="px-5 pb-5">
+              <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                  {product.title}
+                </h5>
+              </a>
+              <div className="flex items-center justify-between mt-3">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                  $ {product.price}
+                </span>
+                <a
+                  href="#"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
