f179c4055c4714fc07b0f29f8caaa62d20ac0b56
ranggaydtm <ranggay67@gmail.com>
2025-04-22 17:01:39 +0700

    create loading product

---
+++ src/app/product/loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoadingProduct = () => {
+  return (
+    <div className="grid grid-cols-5 gap-2 place-items-center mt-5">
+      <div className="w-72 max-w-sm bg-gray-300 rounded-lg shadow-sm my-2 h-96"></div>
+      <div className="w-72 max-w-sm bg-gray-300 rounded-lg shadow-sm my-2 h-96"></div>
+      <div className="w-72 max-w-sm bg-gray-300 rounded-lg shadow-sm my-2 h-96"></div>
+      <div className="w-72 max-w-sm bg-gray-300 rounded-lg shadow-sm my-2 h-96"></div>
+      <div className="w-72 max-w-sm bg-gray-300 rounded-lg shadow-sm my-2 h-96"></div>
+    </div>
+  );
+};
+
+export default LoadingProduct;
