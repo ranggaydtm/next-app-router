afcf4316eee8a7a7f2fbf47b2cd6f775432bef30
ranggaydtm <ranggay67@gmail.com>
2025-04-24 17:10:22 +0700

    setup firebase

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,49 +1,51 @@
+import { retrieveData, retrieveDataById } from "@/lib/firebase/service";
 import { NextResponse, NextRequest } from "next/server";
 
-const data = [
-  {
-    id: 1,
-    title: "Nike Zoom Vomero 5",
-    price: 10000,
-    image:
-      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1401d8b2-3daa-47cc-9c64-be0120375bda/W+NIKE+ZOOM+VOMERO+5.png",
-  },
-  {
-    id: 2,
-    title: "Nike Zoom Vomero 4",
-    price: 20000,
-    image:
-      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0c1e5173-ecc6-4358-aafe-526ca0b12a16/W+NIKE+ZOOM+VOMERO+5.png",
-  },
-  {
-    id: 3,
-    title: "Nike Dunk Low Retro",
-    price: 20000,
-    image:
-      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ae01c2a0-b415-404a-93e3-088b511cb192/NIKE+DUNK+LOW+RETRO.png",
-  },
-  {
-    id: 4,
-    title: "Nike Air Max Dn8",
-    price: 20000,
-    image:
-      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0d641577-64b4-4963-92d5-2054a2f6de58/AIR+MAX+DN8+AMD.png",
-  },
-  {
-    id: 5,
-    title: "Nike Cortez Leather",
-    price: 20000,
-    image:
-      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/464ab3ac-0dc0-4fd0-84bd-eae123a2db18/NIKE+CORTEZ.png",
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     title: "Nike Zoom Vomero 5",
+//     price: 10000,
+//     image:
+//       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1401d8b2-3daa-47cc-9c64-be0120375bda/W+NIKE+ZOOM+VOMERO+5.png",
+//   },
+//   {
+//     id: 2,
+//     title: "Nike Zoom Vomero 4",
+//     price: 20000,
+//     image:
+//       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0c1e5173-ecc6-4358-aafe-526ca0b12a16/W+NIKE+ZOOM+VOMERO+5.png",
+//   },
+//   {
+//     id: 3,
+//     title: "Nike Dunk Low Retro",
+//     price: 20000,
+//     image:
+//       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ae01c2a0-b415-404a-93e3-088b511cb192/NIKE+DUNK+LOW+RETRO.png",
+//   },
+//   {
+//     id: 4,
+//     title: "Nike Air Max Dn8",
+//     price: 20000,
+//     image:
+//       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0d641577-64b4-4963-92d5-2054a2f6de58/AIR+MAX+DN8+AMD.png",
+//   },
+//   {
+//     id: 5,
+//     title: "Nike Cortez Leather",
+//     price: 20000,
+//     image:
+//       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/464ab3ac-0dc0-4fd0-84bd-eae123a2db18/NIKE+CORTEZ.png",
+//   },
+// ];
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
 
   if (id) {
-    const detailProduct = data.find((item) => item.id === Number(id));
+    // const detailProduct = data.find((item) => item.id === Number(id));
+    const detailProduct = await retrieveDataById("products", id);
 
     if (detailProduct) {
       return NextResponse.json({
@@ -56,5 +58,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ status: 404, message: "Not found", data: {} });
   }
 
-  return NextResponse.json({ status: 200, message: "success", data });
+  const products = await retrieveData("products");
+
+  return NextResponse.json({ status: 200, message: "success", data: products });
 }
---
+++ src/lib/firebase/init.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8huyC0OpZ097N_CarQm8K1u8MXaC7B_E",
+  authDomain: "next-app-router-5c54a.firebaseapp.com",
+  projectId: "next-app-router-5c54a",
+  storageBucket: "next-app-router-5c54a.firebasestorage.app",
+  messagingSenderId: "43853332143",
+  appId: "1:43853332143:web:fdd078ba14b1c6d0b39e5a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
