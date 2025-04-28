2c62c91a55eed2ec6e008605484be9d0a6fc744e
ranggaydtm <ranggay67@gmail.com>
2025-04-28 15:05:51 +0700

    adjustment dashboard page

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function DashboardPage() {
+  const { data: session, status }: { data: any; status: string } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    } else if (session !== undefined && session?.user.role !== "admin") {
+      router.push("/");
+    }
+  }, [router, session, session?.user.role, status]);
+
   return (
     <div className="h-96 flex justify-center items-center bg-gray-300 rounded-xl">
       <h1>Dashboard</h1>
